c26d06c9d167eb704e5c8d209e7875823e84394d
Breno Cardoso <elisiobreno@gmail.com>
2024-09-05 22:50:44 -0300

    Drivers Services

---
+++ ErgastF1/DTO/Drivers.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+
+namespace ErgastF1.DTO
+{
+    public class DriverResponse
+    {
+        [JsonProperty("MRData", NullValueHandling = NullValueHandling.Ignore)]
+        public DriverDTO Data;
+    }
+
+    public class DriverDTO : PaginationDTO
+    {
+        [JsonProperty("DriverTable", NullValueHandling = NullValueHandling.Ignore)]
+        public DriverTable DriverTable;
+    }
+
+    public class DriverTable
+    {
+        [JsonProperty("season", NullValueHandling = NullValueHandling.Ignore)]
+        public string Season;
+
+        [JsonProperty("round", NullValueHandling = NullValueHandling.Ignore)]
+        public string Round;
+
+        [JsonProperty("driverId", NullValueHandling = NullValueHandling.Ignore)]
+        public string DriverId;
+
+        [JsonProperty("Drivers", NullValueHandling = NullValueHandling.Ignore)]
+        public List<Driver> Drivers;
+    }
+
+    public class Driver
+    {
+        [JsonProperty("driverId", NullValueHandling = NullValueHandling.Ignore)]
+        public string DriverId;
+
+        [JsonProperty("permanentNumber", NullValueHandling = NullValueHandling.Ignore)]
+        public string PermanentNumber;
+
+        [JsonProperty("code", NullValueHandling = NullValueHandling.Ignore)]
+        public string Code;
+
+        [JsonProperty("url", NullValueHandling = NullValueHandling.Ignore)]
+        public string Url;
+
+        [JsonProperty("givenName", NullValueHandling = NullValueHandling.Ignore)]
+        public string GivenName;
+
+        [JsonProperty("FamilyName", NullValueHandling = NullValueHandling.Ignore)]
+        public string FamilyName;
+
+        [JsonProperty("dateOfBirth", NullValueHandling = NullValueHandling.Ignore)]
+        public string DateOfBirth;
+
+        [JsonProperty("nationality", NullValueHandling = NullValueHandling.Ignore)]
+        public string Nationality;
+    }
+}
---
+++ ErgastF1/Services/DriversServices.cs
@@ -0,0 +1,40 @@
+﻿using ErgastF1.DTO;
+
+namespace ErgastF1.Services
+{
+    public class DriversServices : Service
+    {
+        public DriversServices() { }
+
+        // ergast.com/api/f1/drivers.json
+        public async Task<DriverDTO> List(int offset = 0, int limit = 10)
+        {
+            string path = "drivers";
+            string query = $"?offset={offset}&limit={limit}";
+            return await SendRequest<DriverDTO>(path, query);
+        }
+
+        // ergast.com/api/f1/{year}/drivers.json
+        public async Task<DriverDTO> ListBySeason(int year, int offset = 0, int limit = 10)
+        {
+            string path = $"{year}/drivers";
+            string query = $"?offset={offset}&limit={limit}";
+            return await SendRequest<DriverDTO>(path, query);
+        }
+
+        // ergast.com/api/f1/{year}/{round}/drivers.json
+        public async Task<DriverDTO> ListByRace(int year, int round, int offset = 0, int limit = 10)
+        {
+            string path = $"{year}/{round}/drivers";
+            string query = $"?offset={offset}&limit={limit}";
+            return await SendRequest<DriverDTO>(path, query);
+        }
+
+        // ergast.com/api/f1/{driverId}/drivers.json
+        public async Task<DriverDTO> FindByID(string id)
+        {
+            string path = $"drivers/{id}";
+            return await SendRequest<DriverDTO>(path);
+        }
+    }
+}
