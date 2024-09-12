d17fb9f2249cf52597a89834322a07343e38b0bd
Breno Cardoso <elisiobreno@gmail.com>
2024-09-12 00:11:35 -0300

    Adding results to RaceDTO

--- ErgastF1/DTO/Race.cs
+++ ErgastF1/DTO/Race.cs
@@ -61,6 +61,9 @@ namespace ErgastF1.DTO
 
         [JsonProperty("Qualifying", NullValueHandling = NullValueHandling.Ignore)]
         public PracticeF1 Qualifying;
+
+        [JsonProperty("Results", NullValueHandling = NullValueHandling.Ignore)]
+        public List<Result> Results;
     }
 
     public class PracticeF1
@@ -70,5 +73,74 @@ namespace ErgastF1.DTO
 
         [JsonProperty("time", NullValueHandling = NullValueHandling.Ignore)]
         public string Time;
+    }
+
+    public class Result
+    {
+        [JsonProperty("number", NullValueHandling = NullValueHandling.Ignore)]
+        public string Number;
+
+        [JsonProperty("position", NullValueHandling = NullValueHandling.Ignore)]
+        public string Position;
+
+        [JsonProperty("positionText", NullValueHandling = NullValueHandling.Ignore)]
+        public string PositionText;
+
+        [JsonProperty("points", NullValueHandling = NullValueHandling.Ignore)]
+        public string Points;
+
+        [JsonProperty("Driver", NullValueHandling = NullValueHandling.Ignore)]
+        public Driver Driver;
+
+        [JsonProperty("Constructor", NullValueHandling = NullValueHandling.Ignore)]
+        public Constructor Constructor;
+
+        [JsonProperty("grid", NullValueHandling = NullValueHandling.Ignore)]
+        public string Grid;
+
+        [JsonProperty("laps", NullValueHandling = NullValueHandling.Ignore)]
+        public string Laps;
+
+        [JsonProperty("status", NullValueHandling = NullValueHandling.Ignore)]
+        public string Status;
+
+        [JsonProperty("Time", NullValueHandling = NullValueHandling.Ignore)]
+        public ResultTime Time;
+
+        [JsonProperty("FastestLap", NullValueHandling = NullValueHandling.Ignore)]
+        public ResultFastestLap FastestLap;
+    }
+
+    public class ResultTime
+    {
+        [JsonProperty("millis", NullValueHandling = NullValueHandling.Ignore)]
+        public string Millis;
+
+        [JsonProperty("time", NullValueHandling = NullValueHandling.Ignore)]
+        public string Time;
+    }
+
+    public class ResultFastestLap
+    {
+        [JsonProperty("rank", NullValueHandling = NullValueHandling.Ignore)]
+        public string Rank;
+
+        [JsonProperty("lap", NullValueHandling = NullValueHandling.Ignore)]
+        public string Lap;
+
+        [JsonProperty("Time", NullValueHandling = NullValueHandling.Ignore)]
+        public ResultTime Time;
+
+        [JsonProperty("AverageSpeed", NullValueHandling = NullValueHandling.Ignore)]
+        public ResultAverageSpeed AverageSpeed;
+    }
+
+    public class ResultAverageSpeed
+    {
+        [JsonProperty("units", NullValueHandling = NullValueHandling.Ignore)]
+        public string Units;
+
+        [JsonProperty("speed", NullValueHandling = NullValueHandling.Ignore)]
+        public string Speed;
     }
 }
